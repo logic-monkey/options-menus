4d0589abd1da63175ba20614f5c94d43a93b4dee
Logic Monkey <logic.monkey@protonmail.com>
2024-12-09 13:17:45 -0600

    Control customization almost working.

--- controls/control_menu.gd
+++ controls/control_menu.gd
@@ -1,1 +1,13 @@
 extends Control
+
+func _ready() -> void:
+	var bb = load("res://blackboard.tres")
+	if not "players" in bb._data or not "buttons" in bb._data: return
+	for p in bb._data.players:
+		for b in bb._data.buttons:
+			var c = preload("res://addons/options-menus/controls/custom_control.tscn")\
+					.instantiate()
+			c.player = p
+			c.control = b
+			%button_list.add_child(c)
+			c.validate_control()
--- controls/custom_control.gd
+++ controls/custom_control.gd
@@ -1,8 +1,43 @@
+@tool
 extends HBoxContainer
 
+@export var player : String = "p1"
+@export var control : String = "jump":
+	get: return control
+	set(v):
+		control = v
+		%name.text = v.capitalize()
+		if is_node_ready(): validate_control()
+var control_name :
+	get : return "%s_%s" % [player, control]
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var bb = load("res://blackboard.tres")
 	if bb and "title_font" in bb: 	
 		%name.add_theme_font_override("font", bb.title_font)
+	await  get_tree().process_frame
+	validate_control()
+	
+func validate_control():
+	var events = InputMap.action_get_events(control_name)
+	if not events: return
+	%button.text = " %s " % events[0].as_text().trim_suffix(" (Physical)")
+
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		%button.text = " ... "
+	else:
+		validate_control()
+
+func _input(event: InputEvent) -> void:
+	if not %button.button_pressed: return
+	if not ((event is InputEventKey and event.is_pressed())or \
+			(event is InputEventMouseButton and event.is_pressed()) or \
+			(event is InputEventJoypadButton and event.is_pressed()) or \
+			event is InputEventJoypadMotion and abs(event.axis_value) > 0.5):
+		return
+	InputMap.action_erase_events(control_name)
+	InputMap.action_add_event(control_name,event)
+	accept_event()
+	%button.button_pressed = false
